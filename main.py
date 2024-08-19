926db609efc6a6d844e83d22521d238e777b5cb5
Katie McLaughlin <katie@glasnt.com>
2024-08-19 14:10:43 +1000

    formatting

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 ## HTMX
 
+
 @app.get("/public")
 def public():
-    """IAM Code to make a service public. """
+    """IAM Code to make a service public."""
     if "public" in request.args:
         return """
             <span id="public-flag">--allow-unauthenticated</span>
@@ -18,23 +19,36 @@ def public():
             <span id="public-iam"><b>Optional</b>: </span>"
             """
 
+
 @app.post("/input")
 def update_input():
-    """ Multi-glob. (Warning: gnarly.)
+    """Multi-glob. (Warning: gnarly.)
 
     Using the multi-swap extension, we can update multiple "(value)N" fields.
     Presumes at most 10 numbered fields, in the form "[field]N".
     """
 
     response = ""
-    for value in ["project", "projectnum", "region", "service", "repo", "branch", "serviceaccount", "pool", "provider"]:
+    for value in [
+        "project",
+        "projectnum",
+        "region",
+        "service",
+        "repo",
+        "branch",
+        "serviceaccount",
+        "pool",
+        "provider",
+    ]:
         if value in request.form:
             data = request.form[value]
             for i in range(10):
                 response += f"<span id='{value}{i}'>{data}</span>"
     return response
 
+
 ## MAIN
+
 
 def get_context(referer):
     context = {}
@@ -42,7 +56,7 @@ def get_context(referer):
 
     # Get information
     github_repo = "/".join(clean_referer.split("/")[0:2])
-    service_name = clean_referer.split('/')[1].replace("_","-")
+    service_name = clean_referer.split("/")[1].replace("_", "-")
 
     # Guess information
     if "tree" in clean_referer or "blob" in clean_referer:
@@ -50,22 +64,26 @@ def get_context(referer):
     else:
         try:
             response = requests.get(f"https://api.github.com/repos/{github_repo}")
-            branch = response.json()['default_branch']
+            branch = response.json()["default_branch"]
         except:
             branch = ""
 
-    context['github_repo'] = github_repo
-    context['service_name']= service_name
-    context['branch'] = branch
-    context['prefill_message'] = True
+    context["github_repo"] = github_repo
+    context["service_name"] = service_name
+    context["branch"] = branch
+    context["prefill_message"] = True
     return context
+
 
 @app.get("/")
 def home():
     # Optionally insert information from header
     context = {}
-    if 'referer' in request.headers and "https://github.com" in request.headers['referer']:
-        context = get_context(request.headers['referer'])
+    if (
+        "referer" in request.headers
+        and "https://github.com" in request.headers["referer"]
+    ):
+        context = get_context(request.headers["referer"])
 
     return render_template("index.html", **context)
 
